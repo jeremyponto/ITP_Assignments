181d65a7820cbc31d17db99443a43b2272aaaa9a
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-11-20 13:23:40 +0800

    Create Individual_Task_1_Part_2_Jeremy_Ponto.py

---
+++ Individual_Task_1_Part_2_Jeremy_Ponto.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 13:58:34 2019
+
+@author: user
+"""
+
+# 1
+degrees = 150
+print('Degrees=', degrees)
+def convert(degrees):
+    return degrees / 180 * 3.14
+radians = convert(degrees)
+print('Radians=', radians)
+
+# 2
+print("Students' scores:")
+student1 = 80.0
+print(student1)
+student2 = 90.0
+print(student2)
+student3 = 66.5
+print(student3)
+average = (student1 + student2 + student3) / 3
+print('Average=', average)
+
+# 3
+class1 = 32
+class2 = 45
+class3 = 51
+print('Number of students in each group:')
+print('Class 1:', class1 // 5)
+print('Class 2:', class2 // 7)
+print('Class 3:', class3 // 6)
+print('Number of students left over:')
+print('Class 1:', class1 % 5)
+print('Class 2:', class2 % 7)
+print('Class 3:', class3 % 6)
+
+# 4
+pi = 3.14
+pie_diameter = 55.4
+pie_radius = pie_diameter / 2
+circumference = 2 * pi * pie_radius
+circumference_msg = "Jimmy's pie has a circumference:"
+print(circumference_msg, circumference)
+
+# 5
+v = 343
+print('Speed (m/s)=', v)
+f = 256
+print('Frequency (Hz)=', f)
+wavelength = v / f
+print('Wavelength (m)=', wavelength)
