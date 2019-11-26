cdc43e9a3e93e11fe122c7513d9c8b84d9c64c82
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-11-26 11:13:27 +0800

    Create Practice_Assorted_Problem_Jeremy_Ponto.py

---
+++ Practice_Assorted_Problem_Jeremy_Ponto.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 10:50:47 2019
+
+@author: user
+"""
+
+# 1
+def List(List=[]):
+    print(List)
+def Tuple(*Tuple):
+    print(Tuple)
+List([3, 5, 7, 23])
+Tuple(3, 5, 7, 23)
+
