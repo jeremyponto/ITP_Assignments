ccaf769b0fb25a8c3f7c3d9e499129ab39a72623
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-11-26 12:06:53 +0800

    Update Practice_Assorted_Problem_Jeremy_Ponto.py

--- Practice_Assorted_Problem_Jeremy_Ponto.py
+++ Practice_Assorted_Problem_Jeremy_Ponto.py
@@ -6,10 +6,23 @@ Created on Tue Nov 26 10:50:47 2019
 """
 
 # 1
-def List(List=[]):
-    print(List)
-def Tuple(*Tuple):
-    print(Tuple)
+def List(List = []):
+    New_List = []
+    for element in List:
+        element = str(element)
+        New_List.append(element)
+    print(New_List)
+    print(tuple(New_List))
 List([3, 5, 7, 23])
-Tuple(3, 5, 7, 23)
 
+# 2
+def translate(text):
+    vowels = ['a', 'i', 'u', 'e', 'o', ' ']
+    letters = ""
+    for letter in text:
+        if letter in vowels:
+            letters = letters + letter
+        else:
+            letters = letters + letter + 'o' + letter
+    print(letters)
+translate("this is fun")
