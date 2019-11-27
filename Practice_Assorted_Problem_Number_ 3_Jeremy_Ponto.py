d23476b016af6d348ec43eb01eff2ed1008846d1
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-11-27 18:03:37 +0800

    Create Practice_Assorted_Problem_Number_ 3_Jeremy_Ponto.py

---
+++ Practice_Assorted_Problem_Number_ 3_Jeremy_Ponto.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 17:01:22 2019
+
+@author: user
+"""
+
+import calendar
+year_month = [2019, 11]
+print(calendar.month(year_month[0], year_month[1], w=0, l=0))
