02cfe83079e41e2ed255bc3f2d074acfddb20d11
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-11-27 18:03:45 +0800

    Create Practice_Assorted_Problem_Number_ 7_Jeremy_Ponto.py

---
+++ Practice_Assorted_Problem_Number_ 7_Jeremy_Ponto.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 17:05:54 2019
+
+@author: user
+"""
+
+word_list = ["I", "want", "to", "eat", "fried", "rice"]
+print(word_list)
+length_list = []
+for word in word_list:
+    length_list.append(len(word))
+print(length_list)
