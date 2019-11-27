82a4d142f4aa8d8e8284506bad774fa601d7d92e
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-11-27 10:09:24 +0800

    Update Practice_Assorted_Problem_Jeremy_Ponto.py

--- Practice_Assorted_Problem_Jeremy_Ponto.py
+++ Practice_Assorted_Problem_Jeremy_Ponto.py
@@ -6,14 +6,11 @@ Created on Tue Nov 26 10:50:47 2019
 """
 
 # 1
-def List(List):
-    New_List = []
-    for element in List:
-        element = str(element)
-        New_List.append(element)
-    print(New_List)
-    print(tuple(New_List))
-List([3, 5, 7, 23])
+data = input("")
+data = data.replace(",", "")
+new_list = data.split()
+print("List :", new_list)
+print("Tuple :", tuple(new_list))
 
 # 2
 def translate(text):
