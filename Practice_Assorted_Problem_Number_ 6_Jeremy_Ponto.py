b5df872dc5703e33d8e46d8a36adfbad54f76fe4
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-11-27 18:03:43 +0800

    Create Practice_Assorted_Problem_Number_ 6_Jeremy_Ponto.py

---
+++ Practice_Assorted_Problem_Number_ 6_Jeremy_Ponto.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 17:05:17 2019
+
+@author: user
+"""
+
+def histogram(lst):
+    for element in lst:
+        print('*' * element)
+histogram([4, 9, 7])
