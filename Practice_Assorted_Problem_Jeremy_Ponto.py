e7beb68efe9ccdcb75572ec00621ea21332673bc
jeremyponto01@gmail.com <jeremyponto01@gmail.com>
2019-12-02 09:13:33 +0700

    Update Practice_Assorted_Problem_Jeremy_Ponto.py

--- Practice_Assorted_Problem_Jeremy_Ponto.py
+++ Practice_Assorted_Problem_Jeremy_Ponto.py
@@ -116,41 +116,28 @@ def char_freq(string):
 char_freq(string)
 
 # 12
-alphabets = []
+import string
+alphabets = string.ascii_lowercase
 sentence = str(input(""))
 sentence = sentence.casefold()
-rot = int(input(""))
-new_alphabets = []
-def init_list(alphabets):
-    for i in range(97, 123, 1):
-        alphabets.append(chr(i))
-def init_shifted_list(rot):
-    x = 0
-    for i in range(97, 123, 1):
-        x = i + rot
-        if x > 122:
-            x = x - 26
-        new_alphabets.append(chr(x))
 def encoder(sentence):
     new_sentence = ""
-    for i in sentence:
-        if i in alphabets:
-            new_sentence += new_alphabets[alphabets.index(i)]
-        else:
-            new_sentence += i
+    for letter in sentence:
+        i = alphabets.index(letter) + 13
+        if i >= 26:
+            i = i - 26
+        new_sentence += alphabets[i]
     return new_sentence
 def decoder(new_sentence):
     sentence = ""
-    for i in new_sentence:
-        if i in alphabets:
-            sentence += alphabets[alphabets.index(i)]
-        else:
-            sentence += i
+    for letter in new_sentence:
+        i = alphabets.index(letter) + 13
+        if i >= 26:
+            i = i - 26
+        sentence += alphabets[i]
     return sentence
-init_list(alphabets)
-init_shifted_list(rot)
-print(new_alphabets)
-print(encoder(sentence))
+new_sentence = encoder(sentence)
+print(new_sentence)
 print(decoder(new_sentence))
 
 # 13
